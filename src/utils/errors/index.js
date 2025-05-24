064e29a8d4066c1565dbdf7665fafcf18257f4fa
smit_mewada <smit.mewada@instinctinnovations.com>
2025-05-24 11:23:17 +0530

    Added create booking repository code

---
+++ src/utils/errors/index.js
@@ -0,0 +1,9 @@
+const ValidationError = require("./validation-error");
+const ServiceError = require("./service-error");
+const AppError = require("./app-error");
+
+module.exports = {
+  ValidationError,
+  ServiceError,
+  AppError
+}
